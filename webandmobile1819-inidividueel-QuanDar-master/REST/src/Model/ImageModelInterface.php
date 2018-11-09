b6bb85dd8acc9b265c059e9eaf7514487041f16d
QuanComm <36763887+QuanComm@users.noreply.github.com>
2018-11-09 11:46:12 +0100

    Master push
    Master push

---
+++ webandmobile1819-inidividueel-QuanDar-master/REST/src/Model/ImageModelInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QuanDar
+ * Date: 05/11/2018
+ * Time: 13:44
+ */
+
+namespace App\Model;
+
+
+interface ImageModelInterface
+{
+    public function getImageById($imageId);
+    public function deleteImageById($imageId);
+    public function postImage($image);
+}
---
+++ webandmobile1819-inidividueel-QuanDar-master/REST/src/Model/MessageModelInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Model;
+
+interface MessageModelInterface
+{
+    public function getAllMessages();
+    public function getAllMessagesWithComments();
+    public function getAllCategories();
+    public function getMessageById($messageId);
+    public function searchMessageByContent($search);
+    public function searchMessageByCategory($category);
+    public function searchMessageByContentAndCategory($content, $category);
+    public function getCommentsByMessageId($messageId);
+    public function postComment($messageId, $comment);
+    public function upVoteMessage($messageId);
+    public function downVoteMessage($messageId);
+    public function deleteMessage($messageId);
+}
